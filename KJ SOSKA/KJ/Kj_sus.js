33468f937d10c4301cd9d5f69d150c9ed60b1d36
Ligrar <132701995+Ligrar@users.noreply.github.com>
2023-05-05 21:13:26 +0300

    Add files via upload

---
+++ KJ SOSKA/KJ/Kj_sus.js
@@ -0,0 +1,8 @@
+console.log('Kj sus')
+document.getElementsByClassName("content")
+let a = document.getElementsByClassName("content_left")[0]
+a.textContent = "Хуй"
+console.log(document.getElementsByClassName("content"))
+console.log(document.getElementsByClassName("content")[0])
+a.style.color = 'violet';
+a.style.fontSize = "540px"; 
